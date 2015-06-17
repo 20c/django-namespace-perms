ebb3206f69ac13c27b8f2cf2477699bd3afa91d1
Stefan Pratter <stefan@20c.com>
2015-06-17 13:13:49 +0000

    impl PermissionedModelSerializer

--- django_namespace_perms/rest.py
+++ django_namespace_perms/rest.py
@@ -1,24 +1,32 @@
-from rest_framework import permissions
-from django_namespace_perms.util import has_perms, obj_to_namespace
+from rest_framework import permissions, serializers
+
+from django_namespace_perms.util import (
+  has_perms, 
+  obj_to_namespace,
+  permissions_apply_to_serialized_model
+)
+
 from django_namespace_perms.constants import PERM_READ, PERM_WRITE 
 import logging
+
 
 log = logging.getLogger(__name__)
 
 class BasePermission(permissions.BasePermission):
 
   def debug(self, msg):
+    print msg
     log.debug(msg)
   
   def has_permission(self, request, view):
-    log.debug("Check permission: %s, %s" % (request.method, view))
+    self.debug("Check permission: %s, %s" % (request.method, view))
     if request.method == "POST":
       return has_perms(request.user, view.model, PERM_WRITE)
     else:
       return True
 
   def has_object_permission(self, request, view, obj):
-    log.debug("Check Object permissions %s, %s, %s" % (
+    self.debug("Check Object permissions %s, %s, %s" % (
       request.method,
       request.user,
       obj_to_namespace(obj)
@@ -28,3 +36,24 @@ class BasePermission(permissions.BasePermission):
       return has_perms(request.user, obj, PERM_READ)
     else:
       return has_perms(request.user, obj, PERM_WRITE)
+
+class PermissionedModelSerializer(serializers.ModelSerializer):
+  def to_representation(self, instance):
+    """
+    Apply permissions to serialized data before sending it out for
+    good
+    """
+    r = super(serializers.ModelSerializer, self).to_representation(instance)
+    req = self.context.get("request",None)
+    user = self.context.get("user")
+
+    if not user and req:
+      user = req.user
+
+    if user:
+      r = permissions_apply_to_serialized_model(
+        self,
+        user,
+        data=r
+      )
+    return r
