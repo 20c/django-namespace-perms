c16df4606478575eb406d64312fd86794a4cfd40
Stefan Pratter <stefan@20c.com>
2015-06-18 12:35:29 +0000

    prep for pypi release

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-
 from setuptools import setup
+
+version = open('config/VERSION').read().strip()
+requirements = open('config/requirements.txt').read().split("\n")
 
 setup(
     name='django-namespace-perms',
-    version=open('config/VERSION').read().rstrip(),
+    version=version,
     author='Twentieth Century',
     author_email='code@20c.com',
     description='granular permission system that allows permissioning for read / write operations all the way down to the field level. Also supports completely arbitrary / custom permission namespaces.',
@@ -13,8 +15,13 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    packages=['django_namespace_perms', 'django_namespace_perms.auth'],
+    packages=[
+      'django_namespace_perms', 
+      'django_namespace_perms.auth'
+    ],
+    url = 'https://github.com/20c/django-namespace-perms',
+    download_url = 'https://github.com/20c/django-namespace-perms/%s'%version,
     include_package_data=True,
-    install_requires=open("requirements.txt").read().split("\n"),
+    install_requires=requires,
     zip_safe=False
 )
