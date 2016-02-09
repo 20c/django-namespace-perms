520453abc9cec468d353ca3996ce7dc4abbebc40
Stefan Pratter <stefan@20c.com>
2016-02-09 19:11:43 +0000

    add PERM_CRUD

--- django_namespace_perms/constants.py
+++ django_namespace_perms/constants.py
@@ -1,12 +1,18 @@
 
 PERM_DENY = 0
+
+# if NSP_MODE is unset or set to "rw", PERM_WRITE will give
+# access to all writes
 PERM_READ = 0x01
 PERM_WRITE = 0x02
 
-#CRUD 
+# if NSP_MODE is set to "crud", permission flags for write operations
+# will be more granular
 PERM_UPDATE = PERM_WRITE
 PERM_CREATE = 0x04
 PERM_DELETE = 0x08
+
+PERM_CRUD = PERM_CREATE | PERM_READ | PERM_UPDATE | PERM_DELETE
 
 PERM_CHOICES = [
   (PERM_READ, "read"),
